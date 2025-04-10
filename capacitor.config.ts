bc4a2bc36a49837cf708d9dedfcb0ce772203a81
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-04-10 12:29:38 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'memory-leak-ng',
+  webDir: 'www'
+};
+
+export default config;
